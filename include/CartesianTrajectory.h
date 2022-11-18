eec5234cf053f5b48498b70a70e7af9ceba8be30
Jon <jonathan.woolfrey@gmail.com>
2022-11-18 13:35:23 +0100

    Seems to be working

--- include/CartesianTrajectory.h
+++ include/CartesianTrajectory.h
@@ -7,11 +7,12 @@
 #ifndef CARTESIANTRAJECTORY_H_
 #define CARTESIANTRAJECTORY_H_
 
-#include <iDynTree/Core/CubicSpline.h>                                                              // Fundamental trajectory object
-#include <iDynTree/Core/SpatialAcc.h>                                                               // Linear and angular acceleration
-#include <iDynTree/Core/Transform.h>                                                                // Position and orientation
-#include <iDynTree/Core/Twist.h>                                                                    // Linear and angular velocity
+#include <iDynTree/Core/CubicSpline.h>                                                              // Fundamental trajectory class
+#include <iostream>                                                                                 // std::cerr and std::cout
 #include <vector>                                                                                   // std::vector
+#include <math.h>                                                                                   // atan2, cos, sin
+#include <yarp/sig/Matrix.h>
+#include <yarp/sig/Vector.h>
 
 class CartesianTrajectory
 {
@@ -66,15 +67,16 @@ CartesianTrajectory::CartesianTrajectory(const std::vector<yarp::sig::Matrix> &p
 				std::cerr << "[ERROR] [CARTESIAN TRAJECTORY] Constructor: "
 					  << "Expected a 4x4 matrix for pose " << i+1 << ", "
 					  << "but it had " << poses[i].rows() << "rows "
-					  << "and " << poses[i].cols << " columns." << std::endl;
+					  << "and " << poses[i].cols() << " columns." << std::endl;
 				break;
 			}
 			
-			points[0][i] = poses[i](0,4);                                               // Get the x position
-			points[1][i] = poses[i](1,4);                                               // Get the y position
-			points[2][i] = poses[i](2,4);                                               // Get the z position
+			points[0][i] = poses[i](0,3);                                               // Get the x position
+			points[1][i] = poses[i](1,3);                                               // Get the y position
+			points[2][i] = poses[i](2,3);                                               // Get the z position
 			
-		 	yarp::sig::Matrix R = poses[i](0,0,2,2);                                    // Get the rotation matrix
+		 	yarp::sig::Matrix R = poses[i].submatrix(0,2,0,2);                          // Get the rotation matrix
+	
 		 	
 		 	double yaw   = atan2(R(1,0),R(0,0));                                        // Extract the yaw angle
 		 	
@@ -96,15 +98,16 @@ CartesianTrajectory::CartesianTrajectory(const std::vector<yarp::sig::Matrix> &p
 		}
 		
 		this->isValid = true;
+	}
 }
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////
  //                          Get the desired state for the given time                              //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 bool CartesianTrajectory::get_state(yarp::sig::Matrix &pose,
-                                    yarp::sig::Vector &vel
+                                    yarp::sig::Vector &vel,
                                     yarp::sig::Vector &acc,
-                                    const double &time)
+                                    const double      &time)
 {
 	if(not this->isValid)
 	{
@@ -129,7 +132,7 @@ bool CartesianTrajectory::get_state(yarp::sig::Matrix &pose,
 		
 		for(int i = 0; i < 3; i++)
 		{
-			pos[i] = this->spline[i  ].evaluatePoint(time  vel[i]  , acc[i]);
+			pos[i] = this->spline[i  ].evaluatePoint(time, vel[i]  , acc[i]);
 			rpy[i] = this->spline[i+3].evaluatePoint(time, vel[i+3], acc[i+3]);
 		}
 		
@@ -144,44 +147,11 @@ bool CartesianTrajectory::get_state(yarp::sig::Matrix &pose,
 		
 		pose[0][1] =-cos(rpy[1])*sin(rpy[2]);
 		pose[1][1] =-sin(rpy[0])*sin(rpy[1])*sin(rpy[2]) + cos(rpy[0])*cos(rpy[2]);
-		pose[2][1] = cos(rpy[0])*sin(rpy[1])*sin(rpy[2]) + sin(rpy[0])*cos(rpy[2]));
+		pose[2][1] = cos(rpy[0])*sin(rpy[1])*sin(rpy[2]) + sin(rpy[0])*cos(rpy[2]);
 		
 		pose[0][2] = sin(rpy[1]);
 		pose[1][2] =-sin(rpy[0])*cos(rpy[1]);
 		pose[2][2] = cos(rpy[0])*cos(rpy[1]);
-		
-		return true;
-	}
-} 
-
-bool CartesianTrajectory::get_state(iDynTree::Transform &pose,
-			       	    iDynTree::Twist    &vel,
-			            iDynTree::SpatialAcc &acc,
-			            const double &time)
-{
-	if(not this->isValid)
-	{
-		std::cerr << "[ERROR] [CARTESIANTRAJECTORY] get_state(): "
-			  << "There was a problem during the construction of this object. "
-			  << "Could not get the desired state." << std::endl;
-			  
-		return false;
-	}
-	else
-	{
-		iDynTree::Position pos;
-		iDynTree::Vector3 rpy;
-		iDynTree::GeomVector3 linearVel, angularVel, linearAcc, angularAcc;
-		
-		for(int i = 0; i < 3; i++)
-		{
-			pos[i] = this->spline[i].evaluatePoint(time, linearVel[i], linearAcc[i]);
-			rpy[i] = this->spline[i+3].evaluatePoint(time, angularVel[i], angularAcc[i]);
-		}
-		
-		pose = iDynTree::Transform(iDynTree::Rotation::RPY(rpy[0], rpy[1], rpy[2]), pos);
-		vel = iDynTree::Twist(linearVel, angularVel);
-		acc = iDynTree::SpatialAcc(linearAcc, angularAcc);
 		
 		return true;
 	}
