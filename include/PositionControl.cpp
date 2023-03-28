c564d7b5bc33b20c37ca7cb22a1006544eaa29f3
Jon <jonathan.woolfrey@gmail.com>
2023-03-28 16:26:57 +0200

    Updated to be in line with changes to iCubBase, JointInterface classes

--- include/PositionControl.cpp
+++ include/PositionControl.cpp
@@ -9,7 +9,6 @@ bool PositionControl::threadInit()
 	{
 		std::cout << "[ERROR] [POSITION CONTROL] threadInit(): "
 		          << "A control thread is still running!\n";
-		
 		return false;
 	}
 	else
@@ -18,8 +17,7 @@ bool PositionControl::threadInit()
 		this->isFinished = false;                                                           // New action started
 		this->qRef = this->q;                                                               // Start from current joint position
 		this->startTime = yarp::os::Time::now();                                            // Used to time the control loop
-		
-		return true; // jumps immediately to run()
+		return true;                                                                        // jumps immediately to run()
 	}
 }
 
@@ -28,19 +26,39 @@ bool PositionControl::threadInit()
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 void PositionControl::threadRelease()
 {
-	send_joint_commands(const std::vector<double> this->q.data());                              // Maintain current joint positions
+	send_joint_commands(this->q);                                                               // Maintain current joint positions
 }
 
-/*
   ////////////////////////////////////////////////////////////////////////////////////////////////////
  //                             Compute instantenous position limits                               //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-void PositionControl::compute_joint_limits(double &lower, double &upper, const int &i)
+bool PositionControl::compute_joint_limits(double &lower, double &upper, const unsigned int &jointNum)
 {
-	// NOTE TO SELF: I need to update this with limits on the step size (i.e. velocity)
+	// NOTE TO FUTURE SELF: Need to compute a limit on the step size dq 
 	
-	lower = this->positionLimit[i][0] - this->qRef[i];
-	upper = this->positionLimit[i][1] - this->qRef[i];
+	if(jointNum > this->numJoints)
+	{
+		std::cerr << "[ERROR] [POSITION CONTROL] compute_joint_limits(): "
+		          << "Range of joint indices is 0 to " << this->numJoints - 1 << ", "
+		          << "but you called for " << jointNum << ".\n";
+
+		return false;
+	}
+	else
+	{
+		lower = this->positionLimit[jointNum][0] - this->qRef[jointNum];
+		upper = this->positionLimit[jointNum][1] - this->qRef[jointNum];
+		
+		if(lower >= upper)
+		{
+			std::cerr << "[ERROR] [POSITION CONTROL] compute_joint_limits(): "
+				  << "Lower limit " << lower << " is greater than upper limit " << upper << ". "
+				  << "How did that happen???\n";
+			
+			return false;
+		}
+		else	return true;
+	}
 }
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -48,32 +66,28 @@ void PositionControl::compute_joint_limits(double &lower, double &upper, const i
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 Eigen::Matrix<double,12,1> PositionControl::track_cartesian_trajectory(const double &time)
 {
+	// NOTE TO FUTURE SELF:
+	// There are no checks here to see if the trajectory is queried correctly.
+	// This could cause problems later
+	
 	// Variables used in this scope
 	Eigen::Matrix<double,12,1> dx; dx.setZero();                                                // Value to be returned
 	Eigen::Isometry3d pose;                                                                     // Desired pose
-	Eigen::Matrix<double,6,1> vel, acc;
+	Eigen::Matrix<double,6,1> vel, acc;                                                         // Desired velocity & acceleration
 	
 	if(this->isGrasping)
 	{
-		this->payloadTrajectory.get_state(pose,vel,acc,time);
+		this->payloadTrajectory.get_state(pose,vel,acc,time);                               // Get the desired object state for the given time              
 		
-		dx = this->G.transpose()*( this->dt*vel + this->K*pose_error(pose,this->payload.pose()) );		
+		dx = this->G.transpose()*(this->dt*vel + this->K*pose_error(pose,this->payload.pose())); // Feedforward + feedback control		
 	}
 	else
 	{
-		if(this->leftControl)
-		{
-			this->leftTrajectory.get_state(pose,vel,acc,time);
-			
-			dx.head(6) = this->dt*vel + this->K*pose_error(pose,this->leftPose);
-		}
-		
-		if(this->rightControl)
-		{
-			this->rightTrajectory.get_state(pose,vel,acc,time);
-			
-			dx.tail(6) = this->dt*vel + this->K*pose_error(pose,this->rightPose);
-		}
+		this->leftTrajectory.get_state(pose,vel,acc,time);                                  // Desired state for the left hand
+		dx.head(6) = this->dt*vel + this->K*pose_error(pose,this->leftPose);                // Feedforward + feedback on the left hand
+
+		this->rightTrajectory.get_state(pose,vel,acc,time);                                 // Desired state for the right hand
+		dx.tail(6) = this->dt*vel + this->K*pose_error(pose,this->rightPose);               // Feedforward + feedback on the right hand
 	}
 	
 	return dx;
@@ -90,4 +104,4 @@ Eigen::VectorXd PositionControl::track_joint_trajectory(const double &time)
 	
 	return dq;
 }
-*/
+
