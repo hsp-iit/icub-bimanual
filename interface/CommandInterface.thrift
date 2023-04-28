11230e1dad25098fd2d610c31dcdbd2c9ef02ca7
Jon <jonathan.woolfrey@gmail.com>
2023-04-28 19:07:41 +0200

    Added yarp matrix as a data structure for communication

--- interface/CommandInterface.thrift
+++ interface/CommandInterface.thrift
@@ -1,23 +1,34 @@
+struct Transform
+{
+}
+(
+	yarp.name="yarp::sig::Matrix"
+	yarp.includefile="yarp/sig/Matrix.h"
+)
+
 service CommandInterface
 {
-	bool grasp_object(1:list<double> handPoses);                                                # Grasp object with two hands
+	bool grasp();                                                                               # Activate grasp constraints
 	
-	bool is_finished();                                                                         # Check if the robot has finished excuting a task
+	bool is_finished();                                                                         # Query if the robot has finished moving
 	
-	bool move_object_to_pose(1:list<double> pose,                                               # Move a grasped object to the given pose in the given time
-	                         2:double time);
-	                 
-	bool move_hands_to_pose(1:list<double> poses,                                               # Move the hands to given poses in the givne time
-	                        2:double time);
+	bool perform_cartesian_action(1:string actionName);                                         # Move the hands with a prescribed motion
+	
+	bool perform_grasp_action(1:string actionName);                                             # Move the object with a prescribed action
+	
+	bool perform_joint_space_action(1:string actionName);                                       # Move the joints by a prescribed action
+	
+	bool move_hands_to_pose(1:Transform leftPose,
+	                        2:Transform rightPose,
+	                        3:double time);                                                     # Move the hands to specified poses
+	
+	bool move_joints_to_position(1:list<double> position,
+	                             2:double time);                                                # Move the joints to the given position
 
-	bool move_hands_by_action(1:string actionName);                                             # Move the hands based on a prescribed action
-	
-	bool move_to_configuration(1:list<double> jointConfiguration,
-	                           2:double time);                                                  # Move the joints to a given configuration
-	
-	bool move_to_named_configuration(1:string configName);                                      # Move joints to prescribed configuration
-	
+	bool move_object_to_pose(1:Transform pose,
+	                         2:double time);                                                    # Move the grasped object to a specified pose
+
 	bool release_object();                                                                      # As it says on the label
 	
-	bool stop();                                                                                # Stop the robot moving immediately
+	void stop();                                                                                # Stop the robot moving immediately
 }
