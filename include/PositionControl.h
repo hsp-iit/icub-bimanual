740777bef214fcc510c5cf98051b9b4831b3c71d
Jon <jonathan.woolfrey@gmail.com>
2023-04-28 19:06:18 +0200

    In the middle of debugging problems.

--- include/PositionControl.h
+++ include/PositionControl.h
@@ -51,14 +51,29 @@ class PositionControl : public iCubBase
 	protected:
 		Eigen::VectorXd qRef;                                                               // Reference joint position to send to motors
 
+		Eigen::Matrix<double,12,1> lagrange_multipliers(const Eigen::Matrix<double,12,1> &dx,
+		                                                const Eigen::VectorXd &redundantTask);
+		
 		/////////////////////// Inherited from PeriodicThread class ///////////////////////
 		bool threadInit();
 		void run();
 		void threadRelease();
 		//////////////////////////////////////////////////////////////////////////////////
 		
+		//////////////////////// Methods & members specific to iCub2 //////////////////////
 		Eigen::MatrixXd A;                                                                  // Constraint matrix for the iCub2
 		Eigen::Matrix<double,10,1> b;                                                       // Constraint vector for the iCub2
+		
+		Eigen::VectorXd icub2_cartesian_control(const Eigen::Matrix<double,12,1> &dx,
+		                                        const Eigen::VectorXd &redundantTask,
+		                                        const Eigen::VectorXd &lowerBound,
+		                                        const Eigen::VectorXd &upperBound);
+		
+		Eigen::VectorXd icub2_dls(const Eigen::Matrix<double,12,1> &dx,
+		                          const double &manipulability,
+		                          const Eigen::VectorXd &lowerBound,
+		                          const Eigen::VectorXd &upperBound);
+		
 		
 };                                                                                                  // Semicolon needed after class declaration
 
