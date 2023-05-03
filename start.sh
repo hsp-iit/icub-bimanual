a4121502b22c514242a5d78ae7844320a2cb052f
Jon <jonathan.woolfrey@gmail.com>
2023-05-03 17:19:00 +0200

    Tested launch with ergoCub and iCub2

--- start.sh
+++ start.sh
@@ -3,15 +3,15 @@
 SESSION="bimanual"                                                                       # So we can reference $SESSION later
 
 # Options
-CONFIG="~/workspace/icub-bimanual/config/ergocub.ini"
-PORT="/ergocubSim"
-URDF="~/workspace/robotology-superbuild/src/ergocub-software/urdf/ergoCub/robots/ergoCubGazeboV1/model.urdf"
-WORLD="~/workspace/icub-bimanual/gazebo/worlds/ergocub_grasp_demo.sdf"
+#CONFIG="~/workspace/icub-bimanual/config/ergocub.ini"
+#PORT="/ergocubSim"
+#URDF="~/workspace/robotology-superbuild/src/ergocub-software/urdf/ergoCub/robots/ergoCubGazeboV1/model.urdf"
+#WORLD="~/workspace/icub-bimanual/gazebo/worlds/ergocub_grasp_demo.sdf"
 
-#CONFIG="~/workspace/icub-bimanual/config/icub2.ini"                                                             
-#PORT="/icubSim"                                                                          # Port name
-#URDF="~/workspace/robotology-superbuild/src/icub-models/iCub/robots/iCubGazeboV2_7/model.urdf"
-#WORLD="~/workspace/icub-bimanual/gazebo/worlds/icub2_grasp_demo.sdf"                     # Location of the Gazebo world
+CONFIG="~/workspace/icub-bimanual/config/icub2.ini"                                                             
+PORT="/icubSim"                                                                          # Port name
+URDF="~/workspace/robotology-superbuild/src/icub-models/iCub/robots/iCubGazeboV2_7/model.urdf"
+WORLD="~/workspace/icub-bimanual/gazebo/worlds/icub2_grasp_demo.sdf"                     # Location of the Gazebo world
 
 if pgrep -x gzserver >/dev/null
 	then
