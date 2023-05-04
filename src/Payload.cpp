07bf05efa3c7ac3ce78692b5835db0f3ac885eae
Jon <jonathan.woolfrey@gmail.com>
2023-05-04 12:59:04 +0200

    Updating payload velocity

--- src/Payload.cpp
+++ src/Payload.cpp
@@ -1,9 +1,15 @@
 #include <Payload.h>
 
-void Payload::update_state(const Eigen::Isometry3d &globalToLocal,
-                           const Eigen::Matrix<double,6,1> &twist)
+void Payload::update_state(const Eigen::Isometry3d         &globalToLocal,
+                           const Eigen::Matrix<double,6,1> &contactTwist)
 {
-	this->_globalPose = globalToLocal*this->_localPose;
+	this->_globalPose = globalToLocal*this->_localPose;                                         // Compute the transform of the object in global reference frame
+	
+	Eigen::Vector3d r = _globalPose.translation() - globalToLocal.translation();                // Translation from contact point to center
+	Eigen::Vector3d w = contactTwist.tail(3);
+	
+	this->_twist.head(3) = contactTwist.head(3) + w.cross(r);                                   // v' = v + wxr  linear velocity
+	this->_twist.tail(3) = contactTwist.tail(3);                                                // w' = w'       angular velocity
 }
 
 /*
