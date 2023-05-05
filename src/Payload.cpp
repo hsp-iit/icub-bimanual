e3aa98a8f7749a7abff214efc6bfd6fb28f6908f
Jon <jonathan.woolfrey@gmail.com>
2023-05-05 11:38:58 +0200

    Added function to get twist

--- src/Payload.cpp
+++ src/Payload.cpp
@@ -1,9 +1,15 @@
 #include <Payload.h>
 
 void Payload::update_state(const Eigen::Isometry3d &globalToLocal,
-                           const Eigen::Matrix<double,6,1> &twist)
+                           const Eigen::Matrix<double,6,1> &contactTwist)
 {
-	this->_globalPose = globalToLocal*this->_localPose;
+	this->_globalPose = globalToLocal*this->_localPose;                                         // Compute pose of object in global frame
+	                    
+	Eigen::Vector3d r = this->_globalPose.translation() - globalToLocal.translation();
+	Eigen::Vector3d w = contactTwist.tail(3);
+	
+	this->_twist.head(3) = contactTwist.head(3) + w.cross(r);
+	this->_twist.tail(3) = w;
 }
 
 /*
