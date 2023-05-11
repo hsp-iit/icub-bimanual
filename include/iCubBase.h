9645f3e944410f343ae4d1b3844e3ccf55594a20
Jon <jonathan.woolfrey@gmail.com>
2023-05-11 18:31:30 +0200

    Reworked redundancy resolution to apply singularity avoidance

--- include/iCubBase.h
+++ include/iCubBase.h
@@ -89,6 +89,8 @@ class iCubBase : public QPSolver,
 		
 		enum ControlSpace {joint, cartesian} controlSpace;
 		
+		enum RedundantTask {setPoint, singularityAvoidance} redundantTask = singularityAvoidance;
+		
 		bool isFinished = true;                                                             // For regulating control actions	
 		
 		double startTime, endTime;                                                          // For regulating the control loop
@@ -98,7 +100,7 @@ class iCubBase : public QPSolver,
 		// Kinematics & dynamics
 		Eigen::VectorXd q, qdot;                                                            // Joint positions and velocities
 		
-		Eigen::MatrixXd J, M, invM;                                                         // Jacobian, inertia, and inverse of inertia
+		Eigen::MatrixXd Jleft, Jright, J, M, invM;                                          // Jacobian, inertia, and inverse of inertia
 		
 		iDynTree::Transform basePose;                                                       // Pose of the base
 		
@@ -113,7 +115,9 @@ class iCubBase : public QPSolver,
 		double maxDamping = 0.01;                                                           // For singularity avoidance
 		
 		double threshold  = 0.001;                                                          // For activating singularity avoidance
-		
+	
+		double manipulability;                                                              // Proximity to a singularity
+			
 		Eigen::Matrix<double,6,6> gainTemplate = (Eigen::MatrixXd(6,6) << 1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
 		                                                                  0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
 		                                                                  0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
@@ -124,11 +128,15 @@ class iCubBase : public QPSolver,
 		
 		Eigen::Matrix<double,6,6> D = 2*this->gainTemplate;                                 // Theoretically optimal: kd >= 2*sqrt(kp)
 		
+		double kr = 0.08;                                                                    // Gain for the redundant task
+		
 		CartesianTrajectory leftTrajectory, rightTrajectory;                                // Trajectory generators for the hands
 		
 		Eigen::Isometry3d leftPose, rightPose;                                              // Left and right hand pose
 		
 		Eigen::VectorXd desiredPosition;                                                    // For redundancy resolution
+		
+		Eigen::VectorXd redundant_task();                                                   // Compute the desired redundant task
 		
 		// Grasp control
 		bool isGrasping = false;
@@ -142,8 +150,6 @@ class iCubBase : public QPSolver,
 		CartesianTrajectory payloadTrajectory;                                              // Trajectory generator for a grasped object
 		
 		double graspWidth = 0.0;                                                            // Distance between hands from initial grasp
-		
-		double manipulability_gradient(const double &mu, const Eigen::MatrixXd &J, const unsigned int &jointNum);
 	
 	private:
 		
@@ -155,9 +161,9 @@ class iCubBase : public QPSolver,
 		
 		Eigen::Isometry3d iDynTree_to_Eigen(const iDynTree::Transform &T);                  // Convert iDynTree:Transform to Eigen::Isometry
 		
-		Eigen::Vector3d angle_axis(const Eigen::Matrix3d &R);
+		Eigen::Vector3d angle_axis(const Eigen::Matrix3d &R);                               // Convert rotation matrix to its angle and axis
 		
-		Eigen::MatrixXd partial_derivative(const Eigen::MatrixXd &J, const unsigned int &jointNum);
+		Eigen::MatrixXd partial_derivative(const Eigen::MatrixXd &J, const unsigned int &jointNum); // Get the partial derivative of a Jacobian
 		
 		// NOTE: THESE FUNCTIONS MUST BE DECLARED IN ANY CHILD CLASS OF THIS ONE
 		virtual bool compute_joint_limits(double &lower, double &upper, const unsigned int &jointNum) = 0;
@@ -165,7 +171,6 @@ class iCubBase : public QPSolver,
 		virtual Eigen::VectorXd track_joint_trajectory(const double &time) = 0;
 		
 		virtual Eigen::Matrix<double,12,1> track_cartesian_trajectory(const double &time) = 0;
-		
 	
 		// NOTE: THESE FUNCTIONS ARE FROM THE PERIODICTHREAD CLASS AND NEED TO BE DEFINED
 		// IN THE CHILD CLASSES OF THIS ONE
@@ -176,134 +181,3 @@ class iCubBase : public QPSolver,
 };                                                                                                  // Semicolon needed after class declaration
 
 #endif
-
-/*
-class iCubBase : public yarp::os::PeriodicThread,                                                   // Regulates the control loop
-                 public JointInterface,                                                             // Communicates with motor controllers
-                 public QPSolver                                                                    // Used to solve joint control
-{
-	public:
-		iCubBase(const std::string              &pathToURDF,
-		         const std::vector<std::string> &jointNames,
-		         const std::vector<std::string> &portNames,
-		         const Eigen::Isometry3d        &torsoPose,
-		         const std::string              &robotName);
-		
-		
-		bool is_finished() const { return this->isFinished; }
-		
-		bool set_cartesian_gains(const double &stiffness, const double &damping);
-				       
-		bool set_joint_gains(const double &proportional, const double &derivative);         // As it says on the label
-		
-		void halt();		                                                            // Stops the robot immediately
-		
-		// Joint Control Functions
-		
-		bool move_to_position(const Eigen::VectorXd &position,
-		                      const double &time);
-
-		bool move_to_positions(const std::vector<Eigen::VectorXd> &positions,               // Move joints through multiple positions
-				       const std::vector<double> &times);
-				       		
-		// Cartesian Control Functions
-		
-		bool is_grasping() const { return this->isGrasping; }
-		
-		bool move_to_pose(const Eigen::Isometry3d &leftPose,
-		                  const Eigen::Isometry3d &rightPose,
-		                  const double &time);
-		
-		bool move_to_poses(const std::vector<Eigen::Isometry3d> &leftPoses,
-		                   const std::vector<Eigen::Isometry3d> &rightPoses,
-		                   const std::vector<double> &times);
-		                   
-		bool translate(const Eigen::Vector3d &left,                                         // Translate both hands by the given amount
-		               const Eigen::Vector3d &right,
-		               const double &time);
-		
-		bool move_object(const Eigen::Isometry3d &pose,
-		                 const double &time);
-		                 
-		bool move_object(const std::vector<Eigen::Isometry3d> &poses,
-		                 const std::vector<double> &times);
-				       
-		bool grasp_object(const Payload &_payload);
-
-		bool release_object();
-		               
-		// Information
-				       
-		bool print_hand_pose(const std::string &whichHand);                                 // As it says on the label
-		               
-		Eigen::Isometry3d left_hand_pose()  const { return this->leftPose;  }
-		
-		Eigen::Isometry3d right_hand_pose() const { return this->rightPose; }
-
-	protected:
-	
-		std::string name;                                                                   // As an internal reference
-		
-		Payload payload;                                                                    
-	
-		bool isGrasping = false;
-		bool isFinished = false;
-	
-		double dt     = 0.01;                                                               // Default control frequency
-		double hertz  = 1/this->dt;                                                         // Control frequency
-		double maxAcc = 10;                                                                 // Limits the acceleration
-		double startTime;                                                                   // Used for timing the control loop
-		double endTime;                                                                     // Used for checking when actions are complete
-		
-		Eigen::VectorXd q, qdot;                                                            // Joint positions and velocities
-		
-		enum ControlSpace {joint, cartesian} controlSpace;
-		
-		// Joint control properties
-		double kp = 10.0;                                                                   // Feedback on joint position error
-		double kd =  5.0;                                                                   // Feedback on joint velocity error
-		std::vector<iDynTree::CubicSpline> jointTrajectory;                                 // Generates reference trajectories for joints
-		
-		// Cartesian control
-		bool leftControl, rightControl;                                                     // Switch for activating left and right control
-		CartesianTrajectory leftTrajectory, rightTrajectory;                                // Individual trajectories for left, right hand
-		CartesianTrajectory payloadTrajectory;
-		Eigen::Matrix<double,6,6> K;                                                        // Feedback on pose error
-		Eigen::Matrix<double,6,6> D;                                                        // Feedback on velocity error
-		Eigen::Matrix<double,6,6> gainTemplate;                                             // Structure for the Cartesian gains
-		Eigen::MatrixXd J;                                                                  // Jacobian for both hands
-		Eigen::MatrixXd M;                                                                  // Inertia matrix
-		Eigen::MatrixXd Minv;                                                               // Inverse of the inertia
-		Eigen::Isometry3d leftPose, rightPose;                                              // Pose of the left and right hands
-		
-		// Grasping
-		Eigen::Matrix<double,6,12> G, C;                                                    // Grasp and constraint matrices
-		
-		// Kinematics & dynamics
-		iDynTree::KinDynComputations computer;                                              // Does all the kinematics & dynamics
-		iDynTree::Transform          _torsoPose;                                            // Needed for inverse dynamics; not used yet
-			                       	
-		// Internal functions
-				
-		bool update_state();                                                                // Get new joint state, update kinematics
-		
-		Eigen::Matrix<double,6,1> pose_error(const Eigen::Isometry3d &desired,
-		                                     const Eigen::Isometry3d &actual);              // Get the error between 2 poses for feedback control
-		                                     
-		Eigen::Isometry3d iDynTree_to_Eigen(const iDynTree::Transform &T);                  // Convert iDynTree::Transform to Eigen::Isometry3d
-		
-
-		
-		// Virtual functions to be overridden in derived class                            
-		
-		virtual void compute_joint_limits(double &lower, double &upper, const int &i) = 0;    
-		virtual Eigen::VectorXd track_joint_trajectory(const double &time) = 0;                 // Solve feedforward + feedback control         
-		virtual Eigen::Matrix<double,12,1> track_cartesian_trajectory(const double &time) = 0;
-		
-		// Functions related to the PeriodicThread class
-		bool threadInit() {return true;}                                                                  // Defined in PositionControl.h
-		void threadRelease() {}                                                             // Defined in PositionContro.h
-                void run() {}                                                                     // Defined in iCub2.h
-
-};                                                                                                  // Semicolon needed after class declaration
-*/
